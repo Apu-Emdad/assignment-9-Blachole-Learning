6eea96a3ff081e74dee65cadad8018f51bb79b21
Apu-Emdad <apuemdad96@gmail.com>
2021-10-05 21:53:28 +0600

    error page updated

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from "react";
+import image from "./images/sad.jpg";
 
 const NotFound = () => {
   return (
     <div>
-      <p>not found</p>
+      <img src={image} alt="d" className="d-block mx-auto" />
+      <h3 className="text-center">Sorry!</h3>
+      <h3 className="text-center">The page you looking for is not found</h3>
     </div>
   );
 };
